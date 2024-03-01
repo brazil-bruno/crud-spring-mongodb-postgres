c800470ed348b0fced92643880cba48892453601
brazil-bruno <bruno.brazil2022@gmail.com>
2024-03-01 13:42:51 -0300

    Product entity implemented

--- api-mongodb/src/main/java/com/bruno/apimongodb/entities/Product.java
+++ api-mongodb/src/main/java/com/bruno/apimongodb/entities/Product.java
@@ -3,6 +3,7 @@ package com.bruno.apimongodb.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "products")
@@ -10,4 +11,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+
+    @Id
+    private String _id;
+    private String name;
+    private String description;
+    private Double price;
 }
