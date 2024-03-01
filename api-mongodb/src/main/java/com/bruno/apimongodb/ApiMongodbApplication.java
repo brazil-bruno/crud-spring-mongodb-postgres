19780ce9639a91e9725c30777aee5babc8ab9076
brazil-bruno <bruno.brazil2022@gmail.com>
2024-03-01 13:29:38 -0300

    api-mongodb project created

---
+++ api-mongodb/src/main/java/com/bruno/apimongodb/ApiMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.apimongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMongodbApplication.class, args);
+	}
+
+}
