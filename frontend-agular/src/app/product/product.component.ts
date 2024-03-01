fd93e985586fa7c14df16c2aef642e3beaf63900
brazil-bruno <bruno.brazil2022@gmail.com>
2024-03-01 17:50:16 -0300

    layout table, crud methods created

--- frontend-agular/src/app/product/product.component.ts
+++ frontend-agular/src/app/product/product.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,89 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
+
+  ProductArray: any[] = []
+
+  name: string = "";
+  description: string = "";
+  price: Number = 0;
+  productID = "";
+
+  constructor(private http: HttpClient) {
+    this.getAllProducts();
+  }
+
+  register() {
+    let bodyData = {
+      "name": this.name,
+      "description": this.description,
+      "price": this.price
+    }
+
+    this.http.post("http://localhost:8080/api/products/save", bodyData, { responseType: 'text' })
+      .subscribe((resultData: any) => {
+        console.log(resultData);
+        alert("Product Created");
+        this.getAllProducts();
+
+        this.name = "";
+        this.description = "";
+        this.price = 0;
+      })
+  }
+
+  getAllProducts() {
+    this.http.get("http://localhost:8080/api/products")
+      .subscribe((resultData: any) => {
+        console.log(resultData);
+        this.ProductArray = resultData;
+      })
+  }
+
+  setUpdate(data: any) {
+    this.name = data.name,
+    this.description = data.description,
+    this.price = data.price,
+    this.productID = data._id
+  }
+
+  updateRecords() {
+    let bodyData = {
+      "name": this.name,
+      "description": this.description,
+      "price": this.price
+    }
+
+    this.http.put("http://localhost:8080/api/products/update" + "/" + this.productID, bodyData, {responseType: 'text'})
+      .subscribe((resultData: any) => {
+        console.log(resultData);
+        alert("Product Updated")
+        this.getAllProducts()
+        this.name = ""
+        this.description = ""
+        this.price = 0
+      })
+  }
+
+  save() {
+    if (this.productID == '') {
+      this.register()
+    } else {
+      this.updateRecords()
+    }
+  }
+
+  setDelete(data: any) {
+    this.http.delete("http://localhost:8080/api/products/delete" + "/" + data._id,{responseType: 'text'})
+      .subscribe((resultData: any) => {
+        console.log(resultData);
+        alert("Product Deleted")
+        this.getAllProducts()
+
+        this.name = ""
+        this.description = ""
+        this.price = 0.0
+      })
+  }
 
 }
