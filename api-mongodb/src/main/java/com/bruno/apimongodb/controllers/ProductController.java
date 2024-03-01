414b141518c3cffe4125c094e606e8ffe9aa7cde
brazil-bruno <bruno.brazil2022@gmail.com>
2024-03-01 13:55:12 -0300

    findAllProducts method created

--- api-mongodb/src/main/java/com/bruno/apimongodb/controllers/ProductController.java
+++ api-mongodb/src/main/java/com/bruno/apimongodb/controllers/ProductController.java
@@ -1,9 +1,26 @@
 package com.bruno.apimongodb.controllers;
 
+import com.bruno.apimongodb.entities.Product;
+import com.bruno.apimongodb.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> findAllProducts() {
+        return productService.findAllProducts();
+    }
 }
--- api-mongodb/src/main/java/com/bruno/apimongodb/services/ProductService.java
+++ api-mongodb/src/main/java/com/bruno/apimongodb/services/ProductService.java
@@ -1,7 +1,21 @@
 package com.bruno.apimongodb.services;
 
+import com.bruno.apimongodb.entities.Product;
+import com.bruno.apimongodb.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public List<Product> findAllProducts() {
+        return productRepository.findAll();
+    }
+
 }
