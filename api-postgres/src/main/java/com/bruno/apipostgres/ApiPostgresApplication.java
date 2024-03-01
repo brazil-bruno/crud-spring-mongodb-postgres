05a37c644a69d631fc98a63e40b4bad8d6de1427
brazil-bruno <bruno.brazil2022@gmail.com>
2024-03-01 14:53:57 -0300

    api-postgres project created

---
+++ api-postgres/src/main/java/com/bruno/apipostgres/ApiPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.apipostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPostgresApplication.class, args);
+	}
+
+}
---
+++ api-postgres/src/test/java/com/bruno/apipostgres/ApiPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.apipostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
